8400d8e873d7b855f7bc3bd991be49d886971c21
Johannes Fürmann <johannes@weltraumpflege.org>
2014-01-15 04:43:25 +0100

    first commit, data extraction should work

---
+++ README.md
@@ -0,0 +1,13 @@
+# ioscontacts2vcf #
+
+## What? ##
+
+Simple python script to help you convert your ios contacts to VCF format
+
+## How? ##
+
+t.b.d.
+
+## Who? ##
+
+This is a quick hack by @waaaaargh. If you like it, buy me a beer.
---
+++ ioscontacts2vcf.py
@@ -0,0 +1,79 @@
+import sys
+import argparse
+import sqlite3
+
+class Person:
+    def __init__(self, id, firstname, lastname):
+        self.id = id
+        if firstname is None:
+            if " " in lastname:
+                self.firstname, self.lastname = lastname.split(None, 1)
+            else: 
+                self.firstname, self.lastname = "", lastname
+        elif lastname is None:
+            if " " in firstname:
+                self.firstname, self.lastname = firstname.split(None, 1)
+            else:
+                self.firstname, self.lastname = firstname, ""
+        else:
+            self.firstname, self.lastname = firstname, lastname
+ 
+    @property
+    def vcard(self):
+        return """
+BEGIN:VCARD
+VERSION:3.0
+N:%s,%s
+END:VCARD""" % (self.firstname, self.lastname)
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("infile", help="Path to Contacts database from iDevice")
+    
+    args = arg_parser.parse_args()
+    if args.infile is None:
+        arg_parser.help()
+        sys.exit(-1)
+    
+    
+    ## open database
+    try:
+        conn = sqlite3.connect(args.infile)
+    except sqlite3.Error:
+        print("[!] Could not open database file, are you sure it exists?")
+        sys.exit(-1)
+    try:
+        cursor = conn.cursor()
+        test_query = "SELECT (SELECT COUNT(*) FROM ABPerson), (SELECT COUNT(*) FROM ABMultiValue);"
+        cursor.execute(test_query)
+        result = cursor.fetchone()
+        print("[i] Everything looks good, in fact we found %i entries with %i objects!" % result)
+    except sqlite3.Error as e:
+        print("[!] I could not find the required tables, are you sure there's something in there?") 
+        print(e)
+        sys.exit(-1)
+
+    ## Extract data
+    # query shamelessly stolen from http://www.devkb.org/misc/110-Manual-contact-backup-and-restore-on-iPhone
+    extract_query = "select ROWID, first, last, identifier, value, record_id from ABPerson p join \
+                     ABMultiValue mv on (ROWID=record_id)"
+    
+    persons = []
+
+    try:
+        cursor = conn.cursor()
+        result = cursor.execute(extract_query)
+        for row in result:
+            already_in_list = [x for x in persons if x.id == row[0]]
+            if len(already_in_list) == 0:
+                persons.append(
+                    Person(row[0], row[1], row[2])
+                )
+            else:
+                person = already_in_list[0]
+                
+        for p in persons:
+            print p.vcard
+
+    except sqlite3.Error:
+        print("[!] An unspecified Error occurred")
